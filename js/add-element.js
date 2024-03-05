d12c57b8677c2ee91ff0bdea01dd2ba876e37b32
techwithallie <127265963+techwithallie@users.noreply.github.com>
2024-03-05 14:22:28 -0800

    edits

--- js/add-element.js
+++ js/add-element.js
@@ -1,16 +1,17 @@
 function addItem() {
-var newItem = document.getElementById('items').value;
-// Create a new element and store it in a variable.
-var newEl = document.createElement('li');
+    var newItem = document.getElementById('items').value;
+    // Create a new element and store it in a variable.
+    var newEl = document.createElement('li');
 
-// Create a text node and store it in a variable.
-var newText = document.createTextNode('quinoa');
+    // Create a text node and store it in a variable.
+    var newText = document.createTextNode(newItem); // Use the value from the input field.
 
-// Attach the new text node to the new element.
-newEl.appendChild(newText);
+    // Attach the new text node to the new element.
+    newEl.appendChild(newText);
 
-// Find the position where the new element should be added.
-var position = document.getElementsByTagName('ul')[0];
+    // Find the position where the new element should be added.
+    var position = document.getElementsByTagName('ul')[0];
 
-// Insert the new element into its position.
-position.appendChild(newEl);}
+    // Insert the new element into its position.
+    position.appendChild(newEl);
+}
